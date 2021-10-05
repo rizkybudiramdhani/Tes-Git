2e77233d10065697f48255f871fe89e4e2681d23
rizkybudiramdhani <91581120+rizkybudiramdhani@users.noreply.github.com>
2021-10-05 20:26:05 +0700

    Add files via upload

---
+++ Tes-Git1.py
@@ -0,0 +1,23 @@
+﻿# Aplikasi Input Data Siswa
+
+
+import os
+import sys
+
+class Mahasiswa:
+	nim = ''
+	nama = ''
+
+pilih = 0
+dataSiswa = []
+
+def menu() :
+	os.system('cls')
+	print("Menu Aplikasi Data Siswa")
+	print(30*"-")
+	print("1. Input Data Siswa")
+	print("2. Tampilkan Data siswa")
+	print("3. Update Data Siswa")
+	print("4. Hapus Data siswa")
+	print("5. Author")
+	print("6. keluar Aplikasi")
